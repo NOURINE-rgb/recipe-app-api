8e269f6f9b0bca652dfcb066e67fc2a3e96f0925
yacine <mohamednourine382@gmail.com>
2026-02-05 23:33:28 +0100

    fixing some configs && this is the last touch

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,7 +3,7 @@ Django management command to wait for the database to be available.
 """
 import time
 from django.core.management.base import BaseCommand
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg import OperationalError as PsycopgError
 from django.db.utils import OperationalError
 
 class Command(BaseCommand):
@@ -18,8 +18,7 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2Error, OperationalError):
+            except (PsycopgError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
-        
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,7 +3,7 @@ test django management command.
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg import OperationalError as PsycopgError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -12,24 +12,23 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
-    
+
     def test_wait_for_db_ready(self,patched_check):
         """Test waiting for db when db is available."""
         patched_check.return_value = True
-        
+
         call_command('wait_for_db')
-        
+
         patched_check.assert_called_once_with(databases=['default'])
-        
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for db when db when getting operationalError."""
-        
-        patched_check.side_effect = [Psycopg2Error]*2 + \
+
+        patched_check.side_effect = [PsycopgError]*2 + \
             [OperationalError]*3 + [True]
-        
+
         call_command('wait_for_db')
-        
+
         self.assertEqual(patched_check.call_count,6)
         patched_check.assert_called_with(databases=['default'])
-        
